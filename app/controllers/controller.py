f2abfff6a9a56a2f5bed20dc7c359c703f6f63f2
StuMcA <stuart.mcara@outlook.com>
2021-03-12 12:41:39 +0000

    basic game works

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect
 from app import app
-from app.models.list_of_players import player_1, player_2
+from app.models.list_of_players import player_1, player_2, set_choices
 from app.models.player import Player
 
-@app.route('/index')
+@app.route('/game')
 def index():
     return render_template(
         'index.html', 
@@ -12,38 +12,45 @@ def index():
         player_2=player_2
     )
 
-@app.route('index/rock/rock')
+@app.route('/game/fight', methods=['POST'])
+def fight():
+    p1_choice = request.form['p1_choice']
+    p2_choice = request.form['p2_choice']
+    set_choices(p1_choice, p2_choice)
+    return redirect(f"/game/{player_1.choice}/{player_2.choice}")
+
+@app.route('/game/rock/rock')
 def rock_rock():
     return "Draw!"
 
-@app.route('index/rock/paper')
+@app.route('/game/rock/paper')
 def rock_paper():
     return "P2 wins!"
 
-@app.route('index/rock/scissors')
+@app.route('/game/rock/scissors')
 def rock_scissors():
     return "P1 wins!"
 
-@app.route('index/paper/rock')
+@app.route('/game/paper/rock')
 def paper_rock():
     return "P1 wins!"
 
-@app.route('index/paper/paper')
+@app.route('/game/paper/paper')
 def paper_paper():
     return "Draw!"
 
-@app.route('index/paper/scissors')
+@app.route('/game/paper/scissors')
 def paper_scissors():
     return "P1 wins!"
 
-@app.route('index/scissors/rock')
+@app.route('/game/scissors/rock')
 def scissors_rock():
     return "P2 wins!"
 
-@app.route('index/scissors/paper')
+@app.route('/game/scissors/paper')
 def scissors_paper():
     return "P1 wins!"
 
-@app.route('index/scissors/scissors')
+@app.route('/game/scissors/scissors')
 def scissors_scissors():
     return "Draw!"
--- app/models/list_of_players.py
+++ app/models/list_of_players.py
@@ -2,3 +2,7 @@ from app.models.player import Player
 
 player_1 = Player("Abraham Lincoln", 1)
 player_2 = Player("Jefferson Davis", 2)
+
+def set_choices(p1_choice, p2_choice):
+    player_1.choice = p1_choice
+    player_2.choice = p2_choice
